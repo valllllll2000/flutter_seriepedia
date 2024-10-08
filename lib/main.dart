ae3ad19242fc610cfa475a39e39bc996bf88b784
valllllll2000 <valerie.cardoso@gmail.com>
2024-10-08 15:23:48 +0200

    env keys configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'config/theme/app_theme.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MainApp());
 }
 
