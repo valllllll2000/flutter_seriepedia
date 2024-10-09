611480fde68646179858b4aecf30aedb1ccb06f2
valllllll2000 <valerie.cardoso@gmail.com>
2024-10-09 16:23:42 +0200

    fixed datasource and response typos

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -15,7 +15,7 @@ class MoviedbDatasource extends MoviesDataSource {
 
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
-    final response = await dio.get('/movies/now_playing');
+    final response = await dio.get('/movie/now_playing');
     final movieDbResponse = MovieDbResponse.fromJson(response.data);
     final List<Movie> movies = movieDbResponse.results
         .where((moviedb) => moviedb.posterPath != 'no-poster')
